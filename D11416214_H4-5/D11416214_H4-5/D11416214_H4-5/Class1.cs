ddbe70df47dff0e43276bfec8d5ab49cb1c2feca
d11416214-dotcom <d11416214@gs.takming.edu.tw>
2026-03-27 12:12:18 +0800

    Add files via upload

---
+++ D11416214_H4-5/D11416214_H4-5/D11416214_H4-5/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Specialized;
+
+public struct Student 
+{
+	public int StdID;
+	private string Name;
+	
+	public Student (int StdID,string Name)
+	{
+		this.StdID=StdID;
+		this.Name=Name;	
+	}
+	public string GetStudent()
+	{
+		string str;
+		str = StdID.ToString();
+		str += "" + Name;
+
+		return str;
+	}
+}
---
+++ D11416214_H4-5/D11416214_H4-5/D11416214_H4-5/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace D11416214_H4_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student jeo = new Student(1, "AAAA");
+            Student mary = new Student(2, "BBB");
+
+            Console.WriteLine(jeo.ToString());
+            Console.WriteLine(mary.ToString());
+        }
+    }
+}
